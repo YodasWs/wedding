1c01d17c16a328c7804d6f526e92af7e4f1c9cc6
Sam Grundman <samuel@yodas.ws>
2016-05-18 16:13:11 -0400

    Get language for non-Chrome browsers

--- js/page.js
+++ js/page.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
 	$(document).on('contextmenu.img', 'img', false)
-	var lang = navigator.languages[0] || navigator.language
-	if (lang.indexOf('zh') === 0) {
+	if (navigator.language.indexOf('zh') === 0) {
 		$(':lang(zh)').show()
 		$('[lang="en-us"]:not(html)').hide()
-	} else if (lang.indexOf('en-GB') === 0) {
+	} else if (navigator.language.indexOf('en-GB') === 0) {
 		$(':lang(en-GB)').show()
 		$(':lang(en-GB) + [lang="en-us"]').hide()
 	}
