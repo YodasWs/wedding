bc5f460bb50588af31c7208c89c71243e360623c
Sam Grundman <samuel@yodas.ws>
2016-05-30 11:39:37 -0400

    Remove useless Weixin code and add description

--- js/page.js
+++ js/page.js
@@ -9,33 +9,4 @@ $(document).ready(function() {
 		$(':lang(en-GB)').show()
 		$(':lang(en-GB) + [lang="en-us"]').hide()
 	}
-	// Share on WeChat
-	var shareData = {
-//		'appid':appid,
-		'img_url':'http://yodasws.github.io/wedding/img/engagement.jpg',
-		'link':'http://yodasws.github.io/wedding/',
-		'img_width':'639','img_height':'426',
-		'desc':'Wedding. 2016.06.29',
-		'title':'Sam & Jiaxin'
-	}
-	$(document).on('WeixinJSBridgeReady',function(){
-		WeixinJSBridge.on('menu:share:appmessage', function(argv){
-			WeixinJSBridge.invoke('sendAppMessage',shareData,function(res){
-				_report('send_msg',res.err_msg)
-			})
-		})
-		WeixinJSBridge.on('menu:share:timeline', function(argv){
-			WeixinJSBridge.invoke('shareTimeline',shareData,function(res){
-				_report('timeline',res.err_msg)
-			})
-		})
-		WeixinJSBridge.on('menu:share:weibo',function(argv){
-			WeixinJSBridge.invoke('shareWeibo',{
-				'content':shareData.desc,
-				'url':shareData.link,
-			},function(res){
-				_report('weibo',res.err_msg)
-			})
-		}, false)
-	})
 })
